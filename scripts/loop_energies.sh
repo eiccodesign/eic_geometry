a306aa52d87a600215b309607bfafa4d6d0fe08a
rymilton <rmilton@ucla.edu>
2022-09-06 16:46:14 -0700

    Changed default simulation script and energies

--- scripts/loop_energies.sh
+++ scripts/loop_energies.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-# Use either run_sim_hepmc or run_sim_gps
-simulation="${INSERT_PATH}/scripts/run_sim_hepmc.sh"
-energies=(1 2 5 10 20 30 40 50 60 70 80 90 100)
+simulation="${DETECTOR_PATH}/scripts/run_sim_hepmc.sh"
+energies=(10 20 30 40 50 60 70 80 90 100)
 
 for energy in "${energies[@]}"
 do
 	sed -i "s/beam_energy=.*/beam_energy=${energy}/" ${simulation}
 	echo "Starting energy ${energy} GeV"
-	time source ${simulation}
+	time ${simulation}
 done
