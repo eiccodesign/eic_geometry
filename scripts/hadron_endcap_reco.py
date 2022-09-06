a36150b7986ca2a46a83f13cd4b2babe4d83bf44
rymilton <rmilton@ucla.edu>
2022-09-06 16:41:51 -0700

    Added clustering to hcal endcap

--- scripts/hadron_endcap_reco.py
+++ scripts/hadron_endcap_reco.py
@@ -9,30 +9,15 @@ import ROOT
 from Configurables import ApplicationMgr, EICDataSvc, PodioInput, PodioOutput, GeoSvc
 from GaudiKernel.SystemOfUnits import MeV, GeV, mm, cm, mrad
 
-#detector_name = str(os.environ.get("JUGGLER_DETECTOR", "endcapP_insert"))
-#detector_path = str(os.environ.get("DETECTOR_PATH", "."))
-detector_name = "endcapP_insert"
-detector_path = "${INSERT_PATH}"
+detector_name = str(os.environ.get("JUGGLER_DETECTOR", "endcapP_insert"))
+detector_path = str(os.environ.get("DETECTOR_PATH", "${INSERT_PATH}"))
 compact_path = os.path.join(detector_path, detector_name)
 
 # input arguments from calibration file
-#with open(f'{detector_path}/calibrations/emcal_barrel_calibration.json') as f:
-    #calib_data = json.load(f)['electron']
-
-#print(calib_data)
-
-#cb_ecal_sf = float(calib_data['sampling_fraction_img'])
-#scifi_barrel_sf = float(calib_data['sampling_fraction_scfi'])
 
 # get sampling fractions from system environment variable, 1.0 by default
-#ci_ecal_sf = float(os.environ.get("CI_ECAL_SAMP_FRAC", 0.253))
-#cb_hcal_sf = float(os.environ.get("CB_HCAL_SAMP_FRAC", 0.038))
-#ci_hcal_sf = float(os.environ.get("CI_HCAL_SAMP_FRAC", 0.025))
-#ce_hcal_sf = float(os.environ.get("CE_HCAL_SAMP_FRAC", 0.025))
-ci_hcal_sf = "1."
-ci_hcal_insert_sf = "1."
-ci_ecal_sf = "1."
-ci_ecal_insert_sf = "1."
+ci_hcal_sf = float(os.environ.get("CI_HCAL_SAMP_FRAC", 1.))
+ci_hcal_insert_sf = float(os.environ.get("CI_HCAL_INSERT_SAMP_FRAC", 1.))
 
 # input and output
 input_sims = [f.strip() for f in str.split(os.environ["JUGGLER_SIM_FILE"], ",") if f.strip()]
@@ -48,14 +33,9 @@ podioevent = EICDataSvc("EventDataSvc", inputs=input_sims)
 # juggler components
 from Configurables import Jug__Digi__CalorimeterHitDigi as CalHitDigi
 from Configurables import Jug__Reco__CalorimeterHitReco as CalHitReco
-# from Configurables import Jug__Reco__CalorimeterHitsMerger as CalHitsMerger
-# from Configurables import Jug__Reco__CalorimeterIslandCluster as IslandCluster
-
-# from Configurables import Jug__Reco__ImagingPixelReco as ImCalPixelReco
-# from Configurables import Jug__Reco__ImagingTopoCluster as ImagingCluster
-
-# from Configurables import Jug__Reco__ClusterRecoCoG as RecoCoG
-# from Configurables import Jug__Reco__ImagingClusterReco as ImagingClusterReco
+from Configurables import Jug__Reco__CalorimeterHitsMerger as CalHitsMerger
+from Configurables import Jug__Reco__CalorimeterIslandCluster as IslandCluster
+from Configurables import Jug__Reco__ClusterRecoCoG as RecoCoG
 
 from Configurables import Jug__Fast__InclusiveKinematicsTruth as InclusiveKinematicsTruth
 
@@ -63,13 +43,7 @@ from Configurables import Jug__Fast__InclusiveKinematicsTruth as InclusiveKinema
 sim_coll = [
     "MCParticles",
     "HcalEndcapPHits",
-    "HcalEndcapPHitsContributions",
     "HcalEndcapPInsertHits",
-    "HcalEndcapPInsertHitsContributions",
-    "EcalEndcapPHits",
-    "EcalEndcapPHitsContributions",
-    "EcalEndcapPInsertHits",
-    "EcalEndcapPInsertHitsContributions"
 ]
 
 # input and output
@@ -93,6 +67,25 @@ ci_hcal_reco = CalHitReco("ci_hcal_reco",
         samplingFraction=ci_hcal_sf,
         **ci_hcal_daq)
 
+ci_hcal_merger = CalHitsMerger("ci_hcal_merger",
+        inputHitCollection=ci_hcal_reco.outputHitCollection,
+        outputHitCollection="HcalEndcapPHitsRecoXY",
+        readoutClass="HcalEndcapPHits",
+        fields=["layer", "slice"],
+        fieldRefNumbers=[1, 0])
+
+ci_hcal_cl = IslandCluster("ci_hcal_cl",
+        inputHitCollection=ci_hcal_merger.outputHitCollection,
+        outputProtoClusterCollection="HcalEndcapPProtoClusters",
+        splitCluster=False,
+        minClusterCenterEdep=30.*MeV,
+        localDistXY=[15.*cm, 15.*cm])
+
+ci_hcal_clreco = RecoCoG("ci_hcal_clreco",
+        inputProtoClusterCollection=ci_hcal_cl.outputProtoClusterCollection,
+        outputClusterCollection="HcalEndcapPClusters",
+        logWeightBase=6.2)
+
 # Hadron Endcap HCal Insert
 ci_hcal_insert_daq = dict(
          dynamicRangeADC=200.*MeV,
@@ -109,40 +102,24 @@ ci_hcal_insert_reco = CalHitReco("ci_hcal_insert_reco",
         thresholdFactor=0.0,
         samplingFraction=ci_hcal_insert_sf,
         **ci_hcal_insert_daq)
+# ci_hcal_insert_merger = CalHitsMerger("ci_hcal_insert_merger",
+#         inputHitCollection=ci_hcal_insert_reco.outputHitCollection,
+#         outputHitCollection="HcalEndcapPInsertHitsRecoXY",
+#         readoutClass="HcalEndcapPInsertHits",
+#         fields=["layer", "slice"],
+#         fieldRefNumbers=[1, 0])
 
-# Hadron Endcap ECal
-ci_ecal_daq = dict(
-         dynamicRangeADC=200.*MeV,
-         capacityADC=32768,
-         pedestalMean=400,
-         pedestalSigma=10)
-ci_ecal_digi = CalHitDigi("ci_ecal_digi",
-         inputHitCollection="EcalEndcapPHits",
-         outputHitCollection="EcalEndcapHitsDigi",
-         **ci_ecal_daq)
-ci_ecal_reco = CalHitReco("ci_ecal_reco",
-        inputHitCollection=ci_ecal_digi.outputHitCollection,
-        outputHitCollection="EcalEndcapPHitsReco",
-        thresholdFactor=0.0,
-        samplingFraction=ci_ecal_sf,
-        **ci_ecal_daq)
+ci_hcal_insert_cl = IslandCluster("ci_hcal_insert_cl",
+        inputHitCollection=ci_hcal_insert_reco.outputHitCollection,
+        outputProtoClusterCollection="HcalEndcapPInsertProtoClusters",
+        splitCluster=False,
+        minClusterCenterEdep=30.*MeV,
+        localDistXY=[15.*cm, 15.*cm])
 
-# Hadron Endcap ECal Insert
-ci_ecal_insert_daq = dict(
-         dynamicRangeADC=200.*MeV,
-         capacityADC=32768,
-         pedestalMean=400,
-         pedestalSigma=10)
-ci_ecal_insert_digi = CalHitDigi("ci_ecal_insert_digi",
-         inputHitCollection="EcalEndcapPInsertHits",
-         outputHitCollection="EcalEndcapPInsertHitsDigi",
-         **ci_ecal_insert_daq)
-ci_ecal_insert_reco = CalHitReco("ci_ecal_insert_reco",
-        inputHitCollection=ci_ecal_insert_digi.outputHitCollection,
-        outputHitCollection="EcalEndcapPInsertHitsReco",
-        thresholdFactor=0.0,
-        samplingFraction=ci_ecal_insert_sf,
-        **ci_ecal_insert_daq)
+ci_hcal_insert_clreco = RecoCoG("ci_hcal_insert_clreco",
+        inputProtoClusterCollection=ci_hcal_insert_cl.outputProtoClusterCollection,
+        outputClusterCollection="HcalEndcapPInsertClusters",
+        logWeightBase=3.6)
 
 # Truth level kinematics
 truth_incl_kin = InclusiveKinematicsTruth("truth_incl_kin",
@@ -155,17 +132,17 @@ podout.outputCommands = ['drop *',
         'keep MCParticles',
         'keep *Digi',
         'keep *Reco*',
+        'keep *Cluster*',
 	'keep Inclusive*']
 
 ApplicationMgr(
     TopAlg = [podin,
-            ci_hcal_digi, ci_hcal_reco,
-            ci_hcal_insert_digi, ci_hcal_insert_reco,
-            ci_ecal_digi, ci_ecal_reco,
-            ci_ecal_insert_digi, ci_ecal_insert_reco,
+            ci_hcal_digi, ci_hcal_reco, ci_hcal_merger, ci_hcal_cl, ci_hcal_clreco,
+            ci_hcal_insert_digi, ci_hcal_insert_reco, ci_hcal_insert_cl, ci_hcal_insert_clreco,
 	    truth_incl_kin, podout],
     EvtSel = 'NONE',
     EvtMax = n_events,
     ExtSvc = [podioevent],
     OutputLevel=DEBUG
 )
+        
