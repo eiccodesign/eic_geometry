2ab7bf0c4af2e4bdb2ed99e9f392ba995368e205
rymilton <rmilton@ucla.edu>
2022-09-06 16:40:50 -0700

    Added options from command line

--- scripts/run_sim_hepmc.sh
+++ scripts/run_sim_hepmc.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Input simulation parameters
+function print_the_help {
+  echo "USAGE: ${0} -n <nevents> -part <particle> -p <momentum> "
+  echo "  OPTIONS: "
+  echo "    -n,--nevents     Number of events"
+  echo "    -part,--particle particle type"
+  echo "    -p, --momentum   particle momentum (GeV)"
+  exit
+}
+
+# Default simulation parameters
 particle="pi-"
 beam_energy=50
 num_events=1000
@@ -10,14 +19,32 @@ phi_min=0. # in degrees
 phi_max=360. # in degrees 
 physics_list="FTFP_BERT_HP"
 
+while [ True ]; do
+if [ "$1" = "--help" -o "$1" = "-h" ]; then
+   print_the_help
+   shift
+elif [ "$1" = "-part" -o "$1" = "--particle" ]; then
+   particle=$2
+   shift 2 # past argument
+elif [ "$1" = "-n" -o "$1" = "--nevents" ]; then
+   num_events=$2
+   shift 2 # past argument
+elif [ "$1" = "-p" -o "$1" = "--momentum" ]; then
+   beam_energy=$2
+   shift 2 # past argument
+else
+   break
+fi
+done
+
 # Output file names
-info_string="${particle}_${beam_energy}GeV_theta_${theta}deg"
+info_string="${particle}_${beam_energy}GeV_theta_${theta_min}-${theta_max}deg"
 hepmcfile="gen_${info_string}.hepmc"
-simfile="insert_sim_${info_string}.edm4hep.root"
-recofile="insert_reco_${info_string}.edm4hep.root"
+simfile="sim_${info_string}.edm4hep.root"
+recofile="reco_${info_string}.edm4hep.root"
 
 # Generating hepmc file
-root -l -b -q "${INSERT_PATH}/hepmc_generation/gen_particles.cxx+(\
+root -l -b -q "${DETECTOR_PATH}/hepmc_generation/gen_particles.cxx+(\
 ${num_events},\
 \"${hepmcfile}\",\
 \"${particle}\",\
@@ -29,7 +56,7 @@ ${beam_energy})"
 
 # Running simulation
 npsim \
-   --compactFile ${INSERT_PATH}/endcapP_insert.xml \
+   --compactFile ${DETECTOR_PATH}/hadron_endcap.xml \
    --numberOfEvents ${num_events} \
    --physicsList ${physics_list} \
    --inputFiles ${hepmcfile} \
@@ -40,4 +67,4 @@ rm ${hepmcfile}
 
 # Running reconstruction
 export JUGGLER_SIM_FILE=${simfile} JUGGLER_REC_FILE=${recofile} JUGGLER_N_EVENTS=${num_events}
-gaudirun.py ${INSERT_PATH}/scripts/endcapP_insert_reco.py
+gaudirun.py ${DETECTOR_PATH}/scripts/hadron_endcap_reco.py
